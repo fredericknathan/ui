4088c26e3dd80d7dd16c4b774946e5c9e4dd19e5
Frederick Nathan Irmawan <91310022+fredericknathan@users.noreply.github.com>
2025-05-06 17:00:10 +0700

    Update ui.py

--- ui.py
+++ ui.py
@@ -22,7 +22,7 @@ def generate_data(model, data):
        'Date': [],
        'Type': [],
        'Vol': [],
-       'Sales': []
+       'Revenue': []
   }
   df_u = data.drop_duplicates(subset=['bill_date'], keep='first')
   for _, row in df_u.iterrows():
@@ -33,8 +33,8 @@ def generate_data(model, data):
        data_dict['Vol'].append(row['sales_volume'])
        y_pred = model.predict([[row.year, row.month, row.kompetitor, row.asp, row.rbp, row.quarter, row.plc_weight, row.plc_adj_asp, row.regional_ship_to_Bali, row.regional_ship_to_Bengkulu, row.regional_ship_to_Lampung, row.plc_phase_Introduction, row.plc_phase_Growth, row.plc_phase_Maturity, row.plc_adj_sales_lag_1, row.plc_adj_sales_lag_3, row.plc_adj_sales_lag_6, row.plc_adj_sales_lag_12, row.plc_sales_ma_3, row.plc_sales_ma_6, row.price_ratio, row.discount_depth]])[0]
        data_dict['Vol'].append(y_pred)
-       data_dict['Sales'].append(row['sales_volume'] * row['asp'])
-       data_dict['Sales'].append(y_pred * row['asp'])
+       data_dict['Revenue'].append(row['sales_volume'] * row['asp'])
+       data_dict['Revenue'].append(y_pred * row['asp'])
 
   return data_dict
 
@@ -64,8 +64,8 @@ with col1:
     st.plotly_chart(fig)
 
 with col2:
-    st.subheader("Sales")
-    fig = px.bar(data, x='Date', y='Sales', color='Type', barmode='group')
+    st.subheader("Revenue")
+    fig = px.bar(data, x='Date', y='Revenue', color='Type', barmode='group')
     st.plotly_chart(fig)
 
 
